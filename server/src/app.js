343ce41ff10816bd34a786816b760efdb06de52b
Sukesh Acharya <sukeshachar1489@gmail.com>
2026-07-28 20:10:29 +0530

    feat: added cors

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,12 @@ import routes from "./routes/index.js";
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
